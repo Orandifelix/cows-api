c75765d865701566c5a09d1ab25e6de57b4319d7
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-10-03 13:35:44 +0300

    show route

--- app/controllers/cows_controller.rb
+++ app/controllers/cows_controller.rb
@@ -3,4 +3,10 @@ class CowsController < ApplicationController
     cows = Cow.all
     render json: cows
   end
+  def show
+  cow = Cow.find(params[:id])
+  render json: cow
+rescue ActiveRecord::RecordNotFound
+  render json: "Cow not found", status: :not_found
+end
 end
